34338cfb0ed59bb1ec45e317c7b011bf82d760c2
Lukas Scarfe <lukkas.scarfe@gmail.com>
2025-12-11 13:24:59 -0800

    Ran the code

--- cronjob.sh
+++ cronjob.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd /home/lukas/Documents/code_to_run/Google-Scholar
 
+git fetch
+git pull
 uv sync
 uv run scrape.py
 uv run plotting.py
 git add .
 git commit . -m 'Ran the code'
 git push
-git fetch
-git pull
+
